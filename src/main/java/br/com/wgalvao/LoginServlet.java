436de4d51d4ddd9051ad727c958874a327f49f56
Wagner Galvao <galvao.wagner@outlook.com.br>
2021-03-05 17:55:52 -0300

    Step 06 : Your First Post Request

--- src/main/java/br/com/wgalvao/LoginServlet.java
+++ src/main/java/br/com/wgalvao/LoginServlet.java
@@ -14,8 +14,13 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.setAttribute("name", request.getParameter("name"));
-		request.setAttribute("password", request.getParameter("password"));
 		request.getRequestDispatcher("WEB-INF/views/Login.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		request.setAttribute("name", request.getParameter("name"));
+		request.getRequestDispatcher("/WEB-INF/views/Welcome.jsp").forward(request, response);
 	}
 }
